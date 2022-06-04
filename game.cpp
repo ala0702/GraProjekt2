d4e674f971c071e867b2cacdf9897bf8c9c9a05e
ala0702 <ala07022019@gmail.com>
2022-06-04 11:05:57 +0200

    game class

---
+++ game.cpp
@@ -0,0 +1,17 @@
+#include "game.h"
+
+game::game()
+{
+}
+
+game::~game()
+{
+}
+
+void game::update()
+{
+}
+
+void game::render()
+{
+}
---
+++ game.h
@@ -0,0 +1,27 @@
+#pragma once
+
+class game
+{
+private:
+	//window and event
+
+	//private functions
+
+public:
+	//con and des
+	game();
+	~game();
+
+
+	//accessors
+
+	//main update and render
+	void update();
+	void render();
+
+	//main runnning game
+
+	void running();
+
+};
+
