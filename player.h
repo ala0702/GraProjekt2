fca727c0309ededf8277b9d560d5fe93d8aa69dc
ala0702 <ala07022019@gmail.com>
2022-06-04 13:01:58 +0200

    player class

---
+++ player.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+
+
+class player
+{
+private:
+	sf::RectangleShape Rshape;
+
+	//private functions
+
+
+public:
+	//constructor and destructor
+	player();
+	~player();
+
+};
+
